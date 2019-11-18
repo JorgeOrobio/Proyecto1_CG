7c06dc82c25a3046878ec7b88636dd8cec6f860f
joroboz1710 <j.orobio@utp.edu.co>
2019-11-17 20:00:34 -0500

    inicio del trabajo del mapa, falta terminar lo grafco y programarlo

---
+++ Sprites/Proyecto1/Mapa/background.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.0" name="background" tilewidth="64" tileheight="64" tilecount="64" columns="8">
+ <image source="../Complete_sprites/Spaceship_art_pack_larger/Background/background.jpg" width="512" height="512"/>
+</tileset>
